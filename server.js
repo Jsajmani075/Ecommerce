7b90d75e8d45ad3e6e48c39bf8ada6b90eb7136d
Jsajmani075 <jagjotajmani075@gmail.com>
2024-11-10 23:47:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-
+const path = require("path");
 //configure env
 dotenv.config();
 
@@ -111,10 +111,10 @@ app.use(morgan("dev"));
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
-
+app.use(express.static(path.join(__dirname, "./client/build")));
 //rest api
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to ecommerce app</h1>");
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 //PORT
